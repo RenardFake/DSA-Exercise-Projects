da2f8cd52f90ddd0ac57c1372e349fb605761417
RenardFake <rmacorol18@gmail.com>
2023-05-20 07:40:11 +0800

    Create public class inputAllBaseTypes {.java

---
+++ Reinforcement/Excercise1.1/public class inputAllBaseTypes {.java
@@ -0,0 +1,19 @@
+public class inputAllBaseTypes {
+    public static void main(String[] args) {
+        //this practice cause im bored
+        boolean thisIsTrue = true;
+        char thisChar = 'a';
+        byte smallbyte = 23;
+        short shortByte = 150;
+        int integer = 50000;
+        long bigint = 1000000;
+        float floating = 6.24;
+        double doublefloat = 5000000.00;
+        System.out.println("All of these are Base Types");
+        System.out.println("Boolean" + thisIsTrue);
+        System.out.println("char" + thisChar);
+        System.out.println("byte" + smallbyte);
+        System.out.println("short" + shortByte);
+    }
+    
+}
